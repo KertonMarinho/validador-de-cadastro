73f4c081b2f15d2fb05a8f6bccc9273b1aef07b4
Kerton marinho <kerton.marinho@outlook.com>
2022-01-24 20:28:03 -0300

    alteração script.js

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
+let validator  = {
+    handlSubmit:(event)=> {
+        event.preventDefault();
+
+        let send = true;
+
+        let inputs =  form.querySelectorAll('input');
+
+        for(let i=0;i<inputs.length;i++) {
+            let input = inputs[i];
+        }
+        
+        if(send) {
+            form.submit();
+        }
+    }
+    
+    
+}
+
+
+
 let form= document.querySelector('.validator');
-form.addEventListener('submit',validator.handleSubmit);
+form.addEventListener('submit',validator.handleSubmit
